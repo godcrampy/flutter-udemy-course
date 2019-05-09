ac17e7b0a0fa400778ecbcfe012f28ff7c3cc490
godcrampy <sahilbondre@gmail.com>
2019-05-09 15:37:09 +0530

    Split Code in Multiple Files

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course/product_manager.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  List<String> _products = ['Food Tester'];
+class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
@@ -20,33 +12,9 @@ class _MyAppState extends State<MyApp> {
           appBar: AppBar(
             title: Text('EasyList'),
           ),
-          body: Column(children: [
-            Container(
-              margin: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                onPressed: () {
-                  setState(() {
-                    _products.add('Food Heater');
-                  });
-                },
-                child: Text('Add Product'),
-              ),
-            ),
-            Column(
-              children: _products
-                  .map(
-                    (element) => Card(
-                          child: Column(
-                            children: <Widget>[
-                              Image.asset('assets/food.jpg'),
-                              Text(element)
-                            ],
-                          ),
-                        ),
-                  )
-                  .toList(),
-            )
-          ])),
+          body: ProductManager('Food Tester')
+      )
     );
   }
 }
+
