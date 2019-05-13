46620674e3d9c35b5e66143fcd30f56c8698fe16
godcrampy <sahilbondre@gmail.com>
2019-05-13 17:22:29 +0530

    Added Alert boxes and modals

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -5,6 +5,32 @@ class ProductPage extends StatelessWidget {
   final String imageUrl;
   ProductPage(this.title, this.imageUrl);
 
+  _showWarningDialogue(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Are You Sure"),
+            content: Text("This Actin cant be undone"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Cancel'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              FlatButton(
+                child: Text('Delete'),
+                onPressed: () {
+                  Navigator.pop(context);
+                  Navigator.pop(context, true);
+                },
+              )
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -24,11 +50,12 @@ class ProductPage extends StatelessWidget {
                         padding: EdgeInsets.all(10.0),
                         child: Text('Product Page')),
                     RaisedButton(
-                      color: Theme.of(context).accentColor,
-                      textColor: Colors.white,
-                      child: Text('Delete Product'),
-                      onPressed: () => Navigator.pop(context, true),
-                    )
+                        color: Theme.of(context).accentColor,
+                        textColor: Colors.white,
+                        child: Text('Delete Product'),
+                        onPressed: () {
+                          _showWarningDialogue(context);
+                        })
                   ]),
             )));
   }
